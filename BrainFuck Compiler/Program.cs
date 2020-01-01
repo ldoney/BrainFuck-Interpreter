892458528a69e16618e7a77daa0b39219e3d24db
Lincoln Doney <lincolndoney@outlook.com>
2020-01-01 12:56:56 -0500

    Finished the run portion

--- BrainFuck Compiler/Program.cs
+++ BrainFuck Compiler/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace BrainFuck_Compiler
 {
@@ -11,38 +13,49 @@ namespace BrainFuck_Compiler
         readonly static List<char> KeyWords = new List<char>() { '+', '-', '<', '>', '.', ',', '[', ']' };
         static void Main(string[] args)
         {
-            Console.WriteLine(Extensions.FindNext("Hellllo".ToCharArray(), 0, "l", 4));
             /*
+            int[] finds = Extensions.FindAllIndexof(args[1].AsEnumerable<char>(), ']');
+            for (int i = 0; i < finds.Length; i++)
+            {
+                Console.WriteLine(finds[i] + " " + Extensions.FindLast(args[1], finds[i]));
+            }
+            */
+            Run(args);
+        }
+        static void Run(string[] args)
+        {
             string inst = args[0];
             string input = "";
             try
             {
                 string content = Extensions.GetFile(args[1]);
                 //C# code
-                if(args[1].EndsWith("cs"))
+                if (args[1].EndsWith("cs"))
                 {
-                    
+
                 }
                 //BrainFuck code
                 else
                 {
                     input = content;
                 }
-            }catch(IOException e)
+            }
+            catch (IOException e)
             {
                 input = args[1];
             }
-            
-            if(inst.ToLower() == "from")
+
+            if (inst.ToLower() == "from")
             {
                 //Convert BrainFuck to C#
                 //TODO: Implement this feature
-            }else if(inst.ToLower() == "to")
+            }
+            else if (inst.ToLower() == "to")
             {
                 //Convert C# to BrainFuck
                 //TODO: Implemenet this feature
             }
-            else if(inst.ToLower() == "run")
+            else if (inst.ToLower() == "run")
             {
                 unsafe
                 {
@@ -52,120 +65,111 @@ namespace BrainFuck_Compiler
                     //Define the stack (I doubt anyone's going over 30,000 addresses)
                     byte[] stack = new byte[30000];
                     //Define the entry point of the stack to the first address
-                    int nestlevel = 0;
-                    fixed(byte* ptr = &stack[0])
+                    int cursor = 0;
+                    //Loop through the input code
+                    for (int i = 0; i < chinpt.Length; i++)
                     {
-                        //Create a temporary cursor to navigate the stack
-                        byte* cursor = ptr;
-                        //Loop through the input code
-                        for (int i = 0; i < chinpt.Length; i++)
+                        //Just to make it easier for me to debug
+                        char kw = chinpt[i];
+                        //Ensure that the code is not a comment or a space
+                        if (KeyWords.Contains(kw))
                         {
-                            Console.WriteLine(i + "\t" + (char)*ptr);
-                            //Just to make it easier for me to debug
-                            char kw = chinpt[i];
-                            //Ensure that the code is not a comment or a space
-                            if (KeyWords.Contains(kw))
+                            //Easier to read than if statements
+                            switch (kw)
                             {
-                                //Easier to read than if statements
-                                switch (kw)
-                                {
-                                    //Add
-                                    case '+':
-                                        //Increment the address the pointer points to
-                                        ++*ptr;
-                                        break;
-                                    //Subtract
-                                    case '-':
-                                        //Subtract the address the pointer points to
-                                        --*ptr;
-                                        break;
-                                    //Increment pointer
-                                    case '>':
-                                        //Increment pointer to right
+                                //Add
+                                case '+':
+                                    //Increment the address the pointer points to
+                                    stack[cursor]++;
+                                    break;
+                                //Subtract
+                                case '-':
+                                    //Subtract the address the pointer points to
+                                    stack[cursor]--;
+                                    break;
+                                //Increment pointer
+                                case '>':
+                                    //Increment pointer to right
+                                    if (!(cursor>stack.Length))
+                                    {
                                         cursor++;
-                                        break;
-                                    //Decrement pointer
-                                    case '<':
-                                        //Decrement pointer to left
+                                    }
+                                    break;
+                                //Decrement pointer
+                                case '<':
+                                    //Decrement pointer to left
+                                    if(!(cursor <= 0))
+                                    {
                                         cursor--;
-                                        break;
-                                    //if the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
-                                    case '[':
-                                            if(*cursor == 0)
-                                            {
-                                                int next = Extensions.FindNext(chinpt, *cursor, "]", nestlevel);
-                                                if (next == -1)
-                                                {
-                                                    throw new CompileError("Could not find next ]", *cursor);
-                                                }
-                                                
-                                                i += next + 1;
-                                            }else
-                                            {
-                                                nestlevel++;
-                                            }
-                                            break;
-                                    //	if the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
-                                    case ']':
-                                           if(*cursor != 0)
-                                           {
-                                                int last = Extensions.FindPrev(chinpt, *cursor, "[", nestlevel);
-                                                if(last == -1)
-                                                {
-                                                    throw new CompileError("Could not find last [", *cursor);
-                                                }
-                                                i -= last + 1;
-                                           }else
-                                           {
-                                                //Move out of nest
-                                                nestlevel--;
-                                           }
-                                        break;
-                                    //Get user input
-                                    case ',':
-                                        try
+                                    }
+                                    break;
+                                //if the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
+                                case '[':
+                                    if (stack[cursor] == 0)
+                                    {
+                                        int next = Extensions.FindNext(new string(chinpt), i);
+                                        if (next == -1)
                                         {
-                                            //Get input and set it to the user's input
-                                           *ptr = (byte)Console.ReadKey().KeyChar;
+                                            throw new CompileError("Could not find next ]", stack[cursor]);
                                         }
-                                        catch (InvalidOperationException e)
+                                        i = next;
+                                    }
+                                    break;
+                                //	if the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
+                                case ']':
+                                    if (stack[cursor] != 0)
+                                    {
+                                        int last = Extensions.FindLast(new string(chinpt), i);
+                                        if (last == -1)
                                         {
-                                            //If they enter an invalid character, throw an exception
-                                            throw;
+                                            throw new CompileError("Could not find last [", stack[cursor]);
                                         }
-                                        break;
-                                    //Print out whats at the address
-                                    case '.':
-                                        //Write whats at the address
-                                        Console.Write((char)*ptr);
-                                        break;
-                                    default:
-                                        break;
-                                }
+                                        i = last;
+                                    }
+                                    break;
+                                //Get user input
+                                case ',':
+                                    try
+                                    {
+                                        //Get input and set it to the user's input
+                                        stack[cursor] = (byte)Console.ReadKey().KeyChar;
+                                    }
+                                    catch (InvalidOperationException e)
+                                    {
+                                        //If they enter an invalid character, throw an exception
+                                        throw;
+                                    }
+                                    break;
+                                //Print out whats at the address
+                                case '.':
+                                    //Write whats at the address
+                                    Console.Write((char)stack[cursor]);
+                                    break;
+                                default:
+                                    break;
                             }
                         }
                     }
                 }
             }
-            */
         }
-        
 
-    [Serializable]
-    class CompileError : Exception
-    {
-        public CompileError()
+
+        [Serializable]
+        class CompileError : Exception
         {
+            public CompileError()
+            {
+
+            }
+
+            public CompileError(String reason, int index)
+                : base(String.Format("ERROR: position {0} {1}", index, reason))
+            {
+
+            }
 
         }
-
-        public CompileError(String reason, int index)
-            : base(String.Format("ERROR: position {0} {1}", index, reason))
-        {
-
-        }
-
-    }
     }
     public static class Extensions
     {
@@ -177,80 +181,50 @@ namespace BrainFuck_Compiler
         {
             return values.Select((b, i) => object.Equals(b, val) ? i : -1).Where(i => i != -1).ToArray();
         }
-        public static int FindNext(char[] input, int from, string look, int nest)
+        public static int FindLast(String expression, int index)
         {
-
-        }
-        public static int FindNext(char[] input, int from, string look)
-        {
-
-        }
-        public static int FindNext(string input, int from, string look)
-        {
-            return input.FindAllIndexof(look);
-        }
-        public static int FindPrev()
-        {
-
-        }
-        /*
-        //TODO: Improve this method... probably make it recursive
-        public static int FindPrev(char[] input, int from, string look, int nest)
-        {
-            int fr = from;
-            for (int i = 0; i < nest; i++)
+            char[] arr = expression.ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            for (int i = index; i >= 0; i--)
             {
-                fr = FindPrev(input.Subsequence(0, fr), fr, look);
-            }
-            return fr;
-        }
-        public static int FindPrev(char[] input, int from, string look)
-        {
-            string str = "";
-            for (int i = 0; i < input.Length; i++)
-            {
-                str += input[i];
-            }
-            return FindPrev(str, from, look);
-        }
-        public static int FindPrev(string input, int from, string look)
-        {
-            return input.LastIndexOf(look, from);
-        }
-
-        public static int FindNext(char[] input, int from, string look, int nest)
-        {
-            int fr = from;
-            int addend = 0;
-            for (int i = 0; i < nest+1; i++)
-            {
-                input = input.Subsequence(fr, input.Length - fr + 1);
-                fr = FindNext(input, 0, look);
-                if(fr == 0)
+                if (arr[i] == ']')
                 {
-                    addend++;
-                }else
+                    stack.Push(arr[i]);
+                }
+                else if (arr[i] == '[')
                 {
-                    addend += fr;
-
+                    stack.Pop();
+                }
+                if (stack.Count == 0)   
+                {
+                    return i;
                 }
             }
-            return addend;
+            return -1;
         }
-        public static int FindNext(char[] input, int from, string look)
+        public static int FindNext(String expression, int index)
         {
-            string str = "";
-            for (int i = 0; i < input.Length - from; i++)
+            char[] arr = expression.ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            for (int i = index; i < arr.Length; i++)
             {
-                str += input[i + from];
+                if (arr[i] == '[')
+                {
+                    stack.Push(arr[i]);
+                }
+                if (arr[i] == ']')
+                {
+                    stack.Pop();
+                }
+                if (stack.Count == 0)
+                {
+                    return i;// the closing index of parenthesis
+                }
             }
-            return FindNext(str, from, look);
+            return -1;
         }
-        public static int FindNext(string input, int from, string look)
-        {
-             return input.IndexOf(look, from);
-        }
-        */
+        //Used if you want to recieve the file
+
         public static string GetFile(string input)
         {
             var path = input;
